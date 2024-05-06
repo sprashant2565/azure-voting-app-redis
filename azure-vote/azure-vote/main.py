88ddeb17ed2ae360b659dc9bc773030e366e187d
Prashant Singh <139744280+sprashant2565@users.noreply.github.com>
2024-05-06 15:44:32 +0530

    Update main.py

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -54,8 +54,8 @@ def index():
     if request.method == 'GET':
 
         # Get current values
-        vote1 = r.get(button1).decode('utf-8')
-        vote2 = r.get(button2).decode('utf-8')            
+        vote11 = r.get(button1).decode('utf-8')
+        vote12 = r.get(button2).decode('utf-8')            
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
